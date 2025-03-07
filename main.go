03d0495af4d80167eb476caf1d631dcd3156cd3b
Daishan Peng <daishan@acorn.io>
2025-03-06 17:08:11 -0700

    fix

--- main.go
+++ main.go
@@ -109,7 +109,7 @@ func handleSlackEvents(accessToken string) http.HandlerFunc {
 			}
 
 			// Create the request to the Acorn API
-			apiURL := "https://main.acornlabs.com/api/assistants/a1gnhpr/projects/p17zttl/tasks/w1tsqww/run?step=*"
+			apiURL := "https://main.acornlabs.com/api/assistants/a1gnhpr/projects/p17zttl/tasks/w1sgnmc/run?step=*"
 			req, err := http.NewRequest(http.MethodPost, apiURL, bytes.NewBuffer(jsonBody))
 			if err != nil {
 				http.Error(w, "Failed to create API request", http.StatusInternalServerError)
